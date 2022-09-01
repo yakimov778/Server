30434c654f80771d92669c50dc27bcedaa417f11
Vladimir <50272314+yakimov778@users.noreply.github.com>
2022-09-01 22:49:05 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started");
+        int port = 8088;
+
+        ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+
+            final String name = in.readLine();
+
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        }
+    }
+}
